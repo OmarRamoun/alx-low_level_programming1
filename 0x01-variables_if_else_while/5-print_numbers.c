d4aaade12c6a776f04089638dd3a3d216394a68b
OmarRamoun <omartarekramoun@gmail.com>
2023-09-10 10:21:03 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - print num between 0-10
+* Return: 0 success
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
